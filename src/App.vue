6ffa10b2d42f1779e8c5de36df97b4d962bf5a54
iamzero7766 <iamzero777@163.com>
2020-12-23 21:14:02 +0800

    homepage

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <app-header></app-header>
-    <router-view />
+    <div class="router-view">
+      <router-view />
+    </div>
   </div>
 </template>
 
@@ -18,21 +20,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background: #f6f6f6;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .router-view {
+    margin-top: 20px;
   }
 }
 </style>
---
+++ src/views/homePages/homePage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home-page-style">
+    <div class="list-view">
+      <div class="top-title"></div>
+    </div>
+    <div class="info-view">121212</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "homePage",
+  data() {
+    return {
+      titleList: []
+    }
+  },
+  methods: {},
+  created() {}
+};
+</script>
+
+<style lang="scss">
+  .home-page-style {
+    width: 1000px;
+    margin: 0 auto;
+    .list-view {
+      width: 690px;
+      border: 1px solid #ddd;
+      float: left;
+      background-color: #fff;
+      .top-title {
+        width: 100%;
+        height: 60px;
+      }
+    }
+    .info-view {
+      width: 280px;
+      float: left;
+      margin-left: 10px;
+      background-color: #fff;
+    }
+  }
+</style>
